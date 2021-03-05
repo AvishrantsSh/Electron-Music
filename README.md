3a903adb110f93a70a1b6c8d8664c4b18997d8e7
AvishrantsSh <avishrants@gmail.com>
2021-03-05 22:12:06 +0530

    Directory Setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Electron App V2
+Just trying to build something good. You'll see :P
---
+++ main.js
@@ -0,0 +1,45 @@
+import { app, BrowserWindow } from 'electron';
+
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
